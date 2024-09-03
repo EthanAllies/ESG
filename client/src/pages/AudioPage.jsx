37ad818b66fcedc31844a7ded6b5895c4630262f
EthanAllies <ethan.allies@gmail.com>
2024-09-03 14:17:51 +0200

    layout fix on homescreen

--- client/src/pages/AudioPage.jsx
+++ client/src/pages/AudioPage.jsx
@@ -1,9 +1,5 @@
 export default function AudioPage(){
     return(
-<<<<<<< HEAD
-        <div className="">AudioPage</div>
-=======
         <div className="outline">AudioPage</div>
->>>>>>> 739a6ffabfb96ed11c5cc27b1a495f927438cc5b
     )
 }
