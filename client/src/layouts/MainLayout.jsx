63cc62908e1c51540d6a766b52fd62109ce55a72
EthanAllies <ethan.allies@gmail.com>
2024-09-10 11:57:50 +0200

    sidebar fixed to side

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -4,15 +4,16 @@ import { Outlet } from 'react-router-dom'
 
 export default function MainLayout() {
   return (
-    <div className='flex'>
-      <SideBar />
-      <div className='flex flex-col w-screen h-screen'>
-        <div className=' h-[60px] flex items-center justify-end'>
-          <div className='pr-9 pt-5'>
+    <div className='flex flex-row overflow-hidden '>
+      <SideBar/>
+      <div className='flex flex-col w-screen h-screen '>
+        <div className=' h-[60px] flex items-center justify-end px-4  '>
             <button className='btn-signup'>Sign in</button>
-          </div>
         </div>
-        <Outlet />
+        <div className='h-[calc(100%-60px)] overflow-y-auto'>
+          <Outlet />
+        </div>
+        
       </div>
     </div>
   )
