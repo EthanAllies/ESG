ffb96e7daae672405d166ae30497005f78183043
EthanAllies <ethan.allies@gmail.com>
2024-09-18 20:35:02 +0200

    pdfs

--- client/src/pages/PdfPage.jsx
+++ client/src/pages/PdfPage.jsx
@@ -21,36 +21,38 @@ export default function PdfPage() {
         }
       }
     }, [id]);
+    return (
+      <div className="flex flex-col items-center w-full h-full p-4 bg-white">
+        {/* Heading */}
+        <h1 className="text-3xl font-semibold mb-4">Chapters</h1>
+  
+              {/* Search bar */}
+              <input
+                  type="text"
+                  placeholder="Search Chapters..."
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                  className="w-3/4 md:w-1/2 border-gray-200 p-2 drop-shadow-md rounded-full mb-6 text-center"
+              />
+  
+              {/* PDF grid */}
+              <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+                  {filteredPdfs.map(pdf => (
+                      <div
+                          key={pdf._id}
+                          className="bg-white p-4 rounded shadow hover:shadow-lg cursor-pointer"
+                          onClick={() => handlePdfClick(pdf)}
+                      >
+                          <img src={pdf.imgurl} alt={pdf.name} className="w-full h-40 object-contain rounded mb-2" />
+                          <h3 className="text-center font-normal text-sm">{pdf.desc}</h3>
+                      </div>
+                  ))}
+              </div>
+          </div>
+      );
   };
 
-  return (
-    <div className="flex flex-col items-center w-full h-full p-4 bg-white">
-      {/* Heading */}
-      <h1 className="text-3xl font-semibold mb-4">Chapters</h1>
+  
 
-            {/* Search bar */}
-            <input
-                type="text"
-                placeholder="Search Chapters..."
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-                className="w-3/4 md:w-1/2 border-gray-200 p-2 drop-shadow-md rounded-full mb-6 text-center"
-            />
 
-            {/* PDF grid */}
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-                {filteredPdfs.map(pdf => (
-                    <div
-                        key={pdf._id}
-                        className="bg-white p-4 rounded shadow hover:shadow-lg cursor-pointer"
-                        onClick={() => handlePdfClick(pdf)}
-                    >
-                        <img src={pdf.imgurl} alt={pdf.name} className="w-full h-40 object-contain rounded mb-2" />
-                        <h3 className="text-center font-normal text-sm">{pdf.desc}</h3>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
-}
 
